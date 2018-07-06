d7a5169e4de969558d4586071815cd8539c59669
muthuubalakan <muthuumuthu@gmail.com>
2018-07-06 17:44:03 +0200

    avoid header file simple func

--- test/error.c
+++
@@ -1,7 +0,0 @@
-#include "error.h"
-
-void error_message(const char *msg)
-{
-	perror(msg);
-	exit(1)
-}
--- test/error.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROXY_ERROR_H
-#define PROXY_ERROR_H
-
-/* Handles error */
-void error_message(const char *msg);
-
-
-#endif

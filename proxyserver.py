599dbaac3c783a330b782d39f2ad5af6b399b657
muthuubalakan <muthuumuthu@gmail.com>
2019-09-13 17:05:25 +0200

    clean up

--- proxyserver.py
+++ proxyserver.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3	
-import socket	
-import sys	
-import threading	
+#!/usr/bin/env python3
+import socket
+from types import ModuleType
+import sys
+import threading
 import logging
 from collections import OrderedDict, namedtuple
 import asyncio
@@ -27,36 +28,52 @@ class PortInUseException(Exception):
    pass
 
 
-class AsyncTCPConnection:
-   __listen  = 10
-   log = True
+class Socket(socket.socket):
+   def __init__(self, *args, **kwargs):
+      super(Socket, self).__init__(*args, **kwargs)
+      self.sock = None
    
-   def __init__(self, host, port, log=log):
+   def tcp_socket(self):
+      self.sock = Socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      return self.sock
+   
+   def settimeout(self, value):
+      if value > 30:
+         raise ValueError(
+            'Set max timeout 30 seconds.'
+         )
+      return super().settimeout(value)
+   
+   def udp_socket(self):
+      raise NotImplementedError(
+         'implement `udp_socket` if needed'
+      )
+   
+
+class AsyncTCPConnection(Socket):
+   __listen  = 10
+
+   def __init__(self, host, port):
       self.host = host
       try:
          self.port = int(port)
       except ValueError:
          sys.stderr.write('Port should be an integer!\nConnection abort')
          sys.exit(1)
-      
-      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-   
-   def __repr__(self):
-      return f'{type(self).__name__}(connection={self.tcp})'
-      
    async def __aenter__(self):
       try:
-         self.sock.bind((self.host, self.port))
-         self.sock.listen(self.__listen)
+         self.tcp_socket.bind((self.host, self.port))
+         self.tcp_socket.listen(self.__listen)
+         self.settimeout(20)
          logging.info(f"Listening on {self.host}:{self.port}")
-         return self.sock
+         return self.tcp_socket
       except socket.error as err:
-         # just catch the errors which caused by 
+         # just catch the errors which caused by
          # invalid address, insufficient port permisstions
          # raise error with clear message
-         # Ignore others errors as no need to catch exception, just raise it. 
+         # Ignore others errors as no need to catch exception, just raise it.
          if err.errno == errno.EACCES:
             msg = 'Pemission denied! Run the script as an administrator'
             raise DestinationRequired(msg)
@@ -65,11 +82,11 @@ class AsyncTCPConnection:
             raise PortInUseException(msg)
          else:
             raise err
-   
+
    async def __aexit__(self, *args):
-      if self.sock:
+      if self.tcp_socket:
          logging.info("Closing connection")
-         self.sock.__exit__(*args)
+         self.tcp_socket.__exit__(*args)
 
 
 
@@ -88,8 +105,30 @@ class BaseRequest:
       'put':'PUT',
       'delete':'DELETE',
       'connect': 'CONNECT'
-      
+
    })
+   
+   def __init__(self, request, acsii=None):
+      self.header = None
+      self._encoding = acsii if acsii else 'utf-8'
+      self._request = request
+   
+   def get_headers(self):
+      return self._request.decode(self._encoding).split('\r\n')
+         
+   def gethostbyname(self, host):
+      try:
+         return socket.gethostbyname(host)
+      except socket.error:
+         raise DNSLookupError(
+            'Cannot resolve ip address! Dns lookup failed.'
+         )
+      
+   def scheme(self):
+      raise NotImplementedError()
+   
+   def encoding(self):
+      return self._encoding
    
    @property
    def method(self):
@@ -99,34 +138,32 @@ class BaseRequest:
 class Request(BaseRequest):
    """
    TODO: Handles request, prepare headers, Auth, cookies and etc.
-   """ 
-   def __init__(self, request, **kwargs):
-      super().__init__()
-      self.auth_scheme = 'basic'
-      self.decode = base64.b64decode
+   """
+   def __init__(self, request, scheme='basic', decode=base64.b64decode):
+      super().__init__(request)
+      self.auth_scheme = scheme
+      self.decode = decode
+      self._requestline = namedtuple('Line', 'method url protocol')
+      
+   def headers(self) -> list:
       try:
-         self.headers = request.decode('utf-8').split('\r\n')
-      except Exception:
-         raise UnicodeDecodeError
-   
-   @property 
+         return self.get_headers()
+      except Exception as decode_error:
+         raise decode_error(
+            f'Used encoding {self.encoding}'
+         )
+
+   @property
    def extract_request_line(self):
-      request_line = namedtuple('Line', 'method url protocol')
-      data = str(self.headers[0]).split()
-      assert len(data) == 3 
-      # method, url, protocol
-      method = data[0]
-      url = data[1]
-      protocol = data[2]
-      return request_line(method, url, protocol)
-   
+      method, url, protocol, rm = str(self.headers[0]).split()
+      return self._requestline(method, url, protocol)
+
    def getmethod(self):
       return self.extract_request_line.method
-   
+
    @property
    def geturl(self):
-      url = self.extract_request_line.url.split('://')
-      url = url[-1]
+      url = self.extract_request_line.url.split('://')[-1]
       remote_url = url.split(':')
       if len(remote_url) == 2:
          host, port = remote_url
@@ -137,7 +174,7 @@ class Request(BaseRequest):
       else:
          return (str(url), None)
 
-      
+
    def get_auth(self):
       proxy_auth = None
       for string in self.headers:
@@ -147,13 +184,13 @@ class Request(BaseRequest):
       if not proxy_auth:
          return False
       return proxy_auth.split()
-   
+
    def auth_scheme(self):
       assert self.get_auth() > 2
       assert self.get_auth[1].lower() == self.auth_scheme, (
          'Unsupported auth scheme.'
       )
-   
+
    def formatted(self):
       method = self.extract_request_line.method
       url = self.extract_request_line.url
@@ -166,10 +203,10 @@ class Request(BaseRequest):
          else:
             url = f'https://www.{url}'
       host, _ =self.geturl
-      client_rq = f'{method} {url} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: python-requests/2.22.0\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n' 
+      client_rq = f'{method} {url} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: python-requests/2.22.0\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n'
       return client_rq.encode('utf-8')
-   
-   
+
+
    def credentials(self):
       crd = self.get_auth()[-1]
       try:
@@ -177,8 +214,8 @@ class Request(BaseRequest):
       except Exception:
          return (None,
                  None)
-      
-   
+
+
    def base64_decode(self, data):
       try:
          data = self.decode(data).decode()
@@ -186,27 +223,31 @@ class Request(BaseRequest):
          pass
       username, password, *rm = data.split(':')
       return (
-         username, 
+         username,
          password,
       )
-   
+
    def auth_check(self):
       pass
-   
-        
+
+
 class ProxyServer:
    def __init__(self, host, port):
       self.host = host
       self.port = port
-      self.loop = asyncio.get_event_loop()
-      
+      self.sock = None
+
+   def send(self):
+       pass
+
+   def connect(self):
+       pass
+
    def proxy_handler(self, client_socket):
       request = client_socket.recv(MAX_REQUEST_BYTES)
-      print(request)
       r = Request(
          request=request
       )
-      
 
       if not r.method:
          raise InvalidRequest(
@@ -217,26 +258,25 @@ class ProxyServer:
          port = 80
       request = r.formatted()
 
-      print("This si ", host)
       ip = None
       try:
          ip = socket.gethostbyname(host)
       except socket.error:
          raise DNSLookupError
-      
+
       assert ip, (
          'Cannot resolve ip address.'
       )
-       # Remote server socket	
-      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
-      s.connect((ip, port))	
-      s.sendall(request)	
-      while True:	
-          data = s.recv(4096)	
-          if (len(data) > 0):	
-              client_socket.send(data)	
-          else:	
-              break	
+       # Remote server socket
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip, port))
+      s.sendall(request)
+      while True:
+          data = s.recv(4096)
+          if (len(data) > 0):
+              client_socket.send(data)
+          else:
+              break
 
    async def _runprocess(self):
       async with AsyncTCPConnection(host=self.host, port=self.port) as connection:
@@ -246,10 +286,6 @@ class ProxyServer:
             d = threading.Thread(target=self.proxy_handler, args=(client, ))
             d.setDaemon(True)
             d.start()
-               
+
    def start(self):
       self.loop.run_until_complete(self._runprocess())
-
-
-p = ProxyServer(host="localhost", port=9089)
-p.start()
